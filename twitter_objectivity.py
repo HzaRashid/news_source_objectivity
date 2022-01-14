c11ba51f8279fb05dabc8b078082bc4e3925efe0
HzaRashid <74062092+HzaRashid@users.noreply.github.com>
2022-01-14 16:12:30 -0500

    Add files via upload

--- twitter_objectivity.py
+++ twitter_objectivity.py
@@ -151,3 +151,7 @@ st.write("The values under the 'Average Objectivity Scores' (AOS) column are tak
          \n The data from AOS is used for the above chart, and calculating \
           the 'Mean Objectivity Score'.")
 
+if __name__ == '__main__':
+    twitter_handle='Reuters'
+    data = get_tweets_data()
+    print(data)
